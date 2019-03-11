f7c601fb81c1d093c295dc2ce6fca29039d24468
Iddo weiner <iddo.nadav@gmail.com>
2019-03-11 16:05:36 +0200

    Delete GC_content.m

--- MATLAB source code/GC_content.m
+++
@@ -1,9 +0,0 @@
-% function for calculating GC content of a sequence
-function [y]=GC_content(sequence)
-if isnan(sequence)
-    y = NaN;
-else
-y = (length(regexpi(sequence,'g')) + length(regexpi(sequence,'c'))) / ...
-    (length(regexpi(sequence,'g')) + length(regexpi(sequence,'c')) + length(regexpi(sequence,'a')) + length(regexpi(sequence,'t')) );
-end
-end
